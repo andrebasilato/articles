f285f48f62536122fbc1a499fcd393e93438c7f0
André Basilato <andrebasilato@gmail.com>
2025-11-03 14:09:44 -0300

    :construction: adjusting update permission dto and current user dto

--- src/auth/dto/current-user.dto.ts
+++ src/auth/dto/current-user.dto.ts
@@ -1,7 +1,31 @@
 export class CurrentUserDto {
+    /**
+     * ID do usuário
+     * @example 1
+     */
     userId!: number;
+
+    /**
+     * Nome do usuário
+     * @example Andre Basilato
+     */
     name!: string;
+
+    /**
+     * Email do usuário
+     * @example andre@example.com
+     */
     email!: string;
+
+    /**
+     * ID da permissão do usuário
+     * @example 1
+     */
     permissionId!: number;
+
+    /**
+     * Nome da permissão do usuário
+     * @example Admin
+     */
     permissionName!: string;
 }
--- src/permissions/dto/update-permission.dto.ts
+++ src/permissions/dto/update-permission.dto.ts
@@ -1,6 +1,14 @@
 export class UpdatePermissionDto {
 
+    /**
+     * Nome da permissão
+     * @example Admin
+     */
     name?: string;
     
+    /**
+     * Descrição da permissão
+     * @example Permissão de administrador
+     */
     description?: string;
 }
